da333502c6c5ba1ced55b299f2a73ddc1e78d859
Dannie <dannienctu@gmail.com>
2020-10-18 12:55:04 +0800

    add unittest

--- server/app.py
+++ server/app.py
@@ -8,16 +8,19 @@ from src.document_extractor import BertDocumentExtractor
 
 app = Flask(__name__)
 
+
 def _shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+
 @app.route('/shutdown')
 def shutdown():
     _shutdown_server()
     return 'Server shutting down...'
+
 
 @app.route('/')
 def hello_world():
