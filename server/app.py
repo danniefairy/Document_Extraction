16c018ca20b26ff500a14d9cd722611a8fa88bda
Dannie <dannienctu@gmail.com>
2020-11-30 17:19:34 +0800

    update

--- server/app.py
+++ server/app.py
@@ -42,10 +42,6 @@ def result():
         return jsonify(result)
     except Exception as e:
         return jsonify({"english_result": "Error: {}".format(e), "chinese_result": "錯誤: {}".format(e)})
-    #document = request.get_json()['params']['document']
-    #result = document_extractor.run(document)
-    #translated_result = [translat_obj.text for translat_obj in translator.translate(result, dest='zh-tw')]
-    #return jsonify({"english_result": result, "chinese_result": translated_result})
 
 
 @app.route('/', methods=['POST'])
