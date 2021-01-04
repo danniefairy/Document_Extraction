c966c673c224496f557a46a39f3708727298c012
Dannie <dannienctu@gmail.com>
2021-01-04 15:13:57 +0800

    update aws bootstrap script

--- scripts/aws_bootstrap.sh
+++ scripts/aws_bootstrap.sh
@@ -7,10 +7,12 @@ yum install python36 -y
 
 # Install repo dependency
 yum install gcc -y
+python3 -m pip install -U pip
 python3 -m pip install Flask==1.1.1
 python3 -m pip install Flask-Cors==3.0.3
 python3 -m pip install transformers
 python3 -m pip install sentence-transformers==0.3.7.2
 python3 -m pip install googletrans==3.0.0
+python3 -m pip install --upgrade cython
 python3 -m pip install sklearn
 python3 -m pip install nltk
