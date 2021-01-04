b4551ae9bf11bfb0558d5c26a6e8925eebba7ecb
Dannie <dannienctu@gmail.com>
2021-01-04 14:42:10 +0800

    update aws bootstrap script

--- scripts/aws_bootstrap.sh
+++ scripts/aws_bootstrap.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-echo "Update yum"
-yum update -y
-
-echo "Install git and clone repo"
-yum install git -y
-git clone https://github.com/danniefairy/Document_Extraction.git
-
-echo "Install pip, python"
-yum install epel-release -y
-yum install python36-pip -y,
-yum install python36 -y
-
-echo "Install repo dependency"
+# Install repo dependency
 yum install gcc -y
 python3 -m pip install Flask==1.1.1
 python3 -m pip install Flask-Cors==3.0.3
